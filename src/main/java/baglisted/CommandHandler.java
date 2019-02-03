b6c59337ac9a98e2954b1bb768f1f838402c1811
jutsins <jutsins@gmail.com>
2019-02-03 21:47:52 +0100

    Add CommandHandler

---
+++ src/main/java/baglisted/CommandHandler.java
@@ -0,0 +1,51 @@
+package baglisted;
+
+
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandHandler implements CommandExecutor {
+
+    private final Main plugin;
+
+    public CommandHandler(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if (commandSender.isOp()) {
+            if (command.getName().equalsIgnoreCase("gm")) {
+                Player p = (Player) commandSender;
+                switch (args[0]) {
+                    case "0":
+                    case "s":
+                        p.setGameMode(GameMode.SURVIVAL);
+                        return true;
+                    case "1":
+                    case "c":
+                        p.setGameMode(GameMode.CREATIVE);
+                        return true;
+                    case "2":
+                    case "a":
+                        p.setGameMode(GameMode.ADVENTURE);
+                        return true;
+                    case "3":
+                    case "sp":
+                        p.setGameMode(GameMode.SPECTATOR);
+                        return true;
+                    default:
+                        p.sendMessage("invalid argument, possible arguments are: 0, 1, 2, 3, s, c, a, sp");
+                        return false;
+                }
+            }
+        } else {
+            commandSender.sendMessage("You have to be OP to use Baglisted commands.");
+            return false;
+        }
+        return true;
+    }
+}
--- src/main/java/baglisted/Main.java
+++ src/main/java/baglisted/Main.java
@@ -1,17 +1,28 @@
 package baglisted;
+
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    CommandHandler commandHandler = new CommandHandler(this);
 
     @Override
     public void onEnable() {
+
         getLogger().info("onEnable invoked");
+
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return commandHandler.onCommand(sender, command, label, args);
     }
 
     @Override
     public void onDisable() {
         getLogger().info("onDisable invoked");
     }
-
-
 }
