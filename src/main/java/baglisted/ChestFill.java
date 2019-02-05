7f3f6511cace437acc38a07873937fb68ff1743e
Unknown <jutsins@gmail.com>
2019-02-05 17:54:22 +0100

    Add timer

---
+++ src/main/java/baglisted/ChestFill.java
@@ -0,0 +1,19 @@
+package baglisted;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+public class ChestFill {
+    Area pvpArea1 = new Area(-50, 21, -7, 9, 49, 50);
+    World world;
+
+    public String fillChests(){
+        world = Bukkit.getServer().getWorlds().get(0);
+
+
+        return " JA";
+    }
+
+
+
+}
